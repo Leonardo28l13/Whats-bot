35fd300ac10408e5734c55a308cce411fc52a083
Leonardo28l13 <lipao6912@gmail.com>
2025-09-04 11:56:23 -0300

    Update Termux_install.sh

--- Termux_install.sh
+++ Termux_install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 pkg update && yes | pkg upgrade && pkg install unzip -y && pkg install jq -y && pkg install nodejs-lts -y && pkg install git -y && pkg install python -y && pkg install ffmpeg -y && pkg install yarn -y
-CURRENT_VERSION=$(curl --silent "https://api.github.com/repos/victorsouzaleal/lbot-whatsapp/releases/latest" | jq -r .tag_name)
 cd ~  && [ ! -d ".gyp" ] && mkdir ~/.gyp
 cd ~/.gyp && [ -f "include.gypi" ] && rm ~/.gyp/include.gypi
 echo "{
