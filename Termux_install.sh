c871643b46b41cb3560a95a48bb9b726675479da
Leonardo28l13 <lipao6912@gmail.com>
2025-09-05 23:52:49 -0300

    Update Termux_install.sh

--- Termux_install.sh
+++ Termux_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pkg update && yes | pkg upgrade && pkg install unzip -y && pkg install jq -y && pkg install nodejs-lts -y && pkg install git -y && pkg install python -y && pkg install ffmpeg -y && pkg install yarn -y
-CURRENT_VERSION=$(curl --silent "https://api.github.com/repos/victorsouzaleal/lbot-whatsapp/releases/latest" | jq -r .tag_name)
+CURRENT_VERSION=$(curl --silent "https://api.github.com/repos/Leonardo28l13/lbot-whatsapp/releases/latest" | jq -r .tag_name)
 cd ~  && [ ! -d ".gyp" ] && mkdir ~/.gyp
 cd ~/.gyp && [ -f "include.gypi" ] && rm ~/.gyp/include.gypi
 echo "{
@@ -9,6 +9,6 @@ echo "{
 	}
 }" >> ~/.gyp/include.gypi
 rm -rf ~/LBOT &&
-wget https://github.com/Leonardo28l13/lbot-whatsapp/releases/download/Latest/LBOT-v3.4.6.zip -P ~ &&
+wget https://github.com/Leonardo28l13/lbot-whatsapp/releases/download/Latest/LBOT-v3.4.7.zip -P ~ &&
 unzip ~/LBOT-v$CURRENT_VERSION.zip -d ~/LBOT &&
 rm ~/LBOT-v$CURRENT_VERSION.zip
